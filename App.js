c1d3f5ca670556e45e43df3d2fb184d9c3773196
Chayuan <gwenael.rob@gmail.com>
2023-06-13 10:21:04 +0200

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Alert, TouchableOpacity } from 'react-native';
+
 
 export default function App() {
+  function onPress() {
+      Alert.alert(
+        'Title',
+        'Message',
+        [
+            {
+                text: 'Yes',
+                onPress: () => {
+                    console.log('Yes');
+                },
+            },
+            {
+                text: 'No',
+                style: 'cancel',
+            },
+        ],
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <TouchableOpacity onPress={onPress} style={{height: 50, width: 150, backgroundColor: 'lavender'}}>
+        <Text>Press Here</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
